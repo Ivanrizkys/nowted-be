bac4405da7bd96a02911a3ce18541548f9fb54e1
Ivanrizkys <ivnrzkys@gmail.com>
2024-08-03 20:08:24 +0700

    chore: move interface and implementation in single file

--- internal/controller/note_controller.go
+++ internal/controller/note_controller.go
@@ -1,7 +1,45 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"notes/internal/dtos"
+	"notes/internal/helper"
+	"notes/internal/service"
+
+	"github.com/gin-gonic/gin"
+)
 
 type NoteController interface {
 	ImageUpload(c *gin.Context)
+}
+
+type NoteControllerImpl struct {
+	NoteService service.NoteService
+}
+
+func NewNoteController(noteService service.NoteService) NoteController {
+	return &NoteControllerImpl{
+		NoteService: noteService,
+	}
+}
+
+func (cl *NoteControllerImpl) ImageUpload(c *gin.Context) {
+	req := dtos.ImageUploadReq{}
+	err := c.Bind(&req)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	res, err := cl.NoteService.ImageUpload(c.Request.Context(), req)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+	helper.SendResponse(c, dtos.ImageUploadRes{
+		ImageUrl: res.ImageUrl,
+	}, helper.Meta{
+		StatusCode: http.StatusCreated,
+		Message:    "Upload image succesfully",
+	})
 }
--- internal/service/note_service.go
+++ internal/service/note_service.go
@@ -2,9 +2,52 @@ package service
 
 import (
 	"context"
-	"notes/internal/model/client"
+	"errors"
+	"fmt"
+	"notes/internal/constant"
+	"notes/internal/dtos"
+
+	"github.com/cloudinary/cloudinary-go/v2"
+	"github.com/cloudinary/cloudinary-go/v2/api/uploader"
+	"github.com/go-playground/validator/v10"
+	"github.com/google/uuid"
 )
 
 type NoteService interface {
-	ImageUpload(ctx context.Context, data client.ImageUploadReq) (client.ImageUploadRes, error)
+	ImageUpload(ctx context.Context, data dtos.ImageUploadReq) (dtos.ImageUploadRes, error)
+}
+
+type NoteServiceImpl struct {
+	Validate   *validator.Validate
+	Cloudinary *cloudinary.Cloudinary
+}
+
+func NewNoteService(validate *validator.Validate, cloudinary *cloudinary.Cloudinary) NoteService {
+	return &NoteServiceImpl{
+		Validate:   validate,
+		Cloudinary: cloudinary,
+	}
+}
+
+func (s *NoteServiceImpl) ImageUpload(ctx context.Context, data dtos.ImageUploadReq) (dtos.ImageUploadRes, error) {
+	var res dtos.ImageUploadRes
+
+	if data.Image.Size == 0 {
+		return res, errors.Join(constant.ErrBadRequest, errors.New("image url can't be empty"))
+	}
+
+	uuid := uuid.Must(uuid.NewV6())
+	cldRes, err := s.Cloudinary.Upload.Upload(ctx, data.Image, uploader.UploadParams{
+		Folder:   "note",
+		PublicID: fmt.Sprintf("%v", uuid),
+	})
+
+	if err != nil {
+		return res, errors.Join(constant.ErrService, err)
+	}
+
+	res = dtos.ImageUploadRes{
+		ImageUrl: cldRes.SecureURL,
+	}
+	return res, nil
 }
