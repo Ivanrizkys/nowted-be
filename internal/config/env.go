66b7db8eda0a3beafbba280bc329ee417b694d03
Ivanrizkys <ivnrzkys@gmail.com>
2024-08-11 10:53:31 +0700

    chore: load new env

--- internal/config/env.go
+++ internal/config/env.go
@@ -9,3 +9,35 @@ func CoudinaryUrl() string {
 	}
 	return env
 }
+
+func GoogleOAuthClientId() string {
+	env := os.Getenv("GOOGLE_OAUTH_CLIENT_ID")
+	if env == "" {
+		panic("can't load google oauth client id")
+	}
+	return env
+}
+
+func GoogleOAuthClientSecreet() string {
+	env := os.Getenv("GOOGLE_OAUTH_CLIENT_SECRET")
+	if env == "" {
+		panic("can't load google oauth client secreets")
+	}
+	return env
+}
+
+func GoogleOAuthRedirectUrl() string {
+	env := os.Getenv("GOOGLE_OAUTH_REDIRECT_URL")
+	if env == "" {
+		panic("can't load google oauth redirect url")
+	}
+	return env
+}
+
+func PostgresUrl() string {
+	env := os.Getenv("POSTGRESQL_URL")
+	if env == "" {
+		panic("can't load postgresql url")
+	}
+	return env
+}
